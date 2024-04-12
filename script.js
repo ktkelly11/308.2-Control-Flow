8e64251ff04a635868f193641759c91d8aa610f9
Katherine Kelly <ktkelly11@gmail.com>
2024-04-11 21:46:08 -0400

    total area calculations

---
+++ script.js
@@ -0,0 +1,16 @@
+// Part 1: Growing Pains
+const PI = 3.1415;
+const totalRadius = 5;
+const plantRadius = 0.8;
+let plantsWeek1 = 20;
+
+const area = 5 * 5 * PI;
+console.log(area);
+// total area is 78.53750000000001 meters
+
+const plantArea = 0.8 * 0.8;
+console.log(plantArea);
+// Area required per plant is 0.6400000000000001 meters
+
+console.log(plantArea * plantsWeek1);
+// During the first week the 20 plants require 12.800000000000002 meters
