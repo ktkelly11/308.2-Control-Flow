0e7f424f99aab31c3fbe8351ac113d2f2689d439
Katherine Kelly <ktkelly11@gmail.com>
2024-04-11 22:12:52 -0400

    week 3

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ let plantsWeek1 = 20;
 
 const area = 5 * 5 * PI;
 console.log(area);
-// total area is 78.53750000000001 meters
+// total area is 78.54 meters
 
 const plantArea = 0.8 * 0.8;
 console.log(plantArea);
-// Area required per plant is 0.6400000000000001 meters
+// Area required per plant is 0.64 meters
 
 // Week #1
 console.log(plantArea * plantsWeek1);
-// During the first week the 20 plants require 12.800000000000002 meters
+// During the first week the 20 plants require 12.80 meters
 
 // The plants double in number every week!
 // Predict the plant growth after a specific number of weeks.
@@ -24,8 +24,11 @@ console.log(plantsWeek2);
 // 40 plants
 
 console.log(plantArea * plantsWeek2);
-//
+// 25.60 meters
 
 // Week #3
 let plantsWeek3 = plantsWeek2 * 2;
 console.log(plantsWeek3);
+
+console.log(plantArea * plantsWeek3);
+// 51.20 meters
