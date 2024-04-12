6497a56fb2fcbe45f7c3623479a849a7db2e7e43
Katherine Kelly <ktkelly11@gmail.com>
2024-04-11 23:22:28 -0400

    if...else control flow

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 // Part 1: Growing Pains
 const PI = 3.1415;
 const totalRadius = 5;
-const plantRadius = 0.8;
+const plantArea = 0.8;
 let plantsWeek1 = 20;
 
 const area = 5 * 5 * PI;
 console.log(area);
 // total area is 78.54 meters
 
-const plantArea = 0.8 * 0.8;
-console.log(plantArea);
-// Area required per plant is 0.64 meters
-
 // Week #1
-console.log(plantArea * plantsWeek1);
-// During the first week the 20 plants require 12.80 meters
+const plantAreaWeek1 = plantArea * plantsWeek1;
+console.log(plantAreaWeek1);
+// During the first week the 20 plants require 16 meters
 
 // The plants double in number every week!
 // Predict the plant growth after a specific number of weeks.
@@ -23,16 +20,18 @@ let plantsWeek2 = plantsWeek1 * 2;
 console.log(plantsWeek2);
 // 40 plants
 
-console.log(plantArea * plantsWeek2);
-// 25.60 meters
+const plantAreaWeek2 = plantArea * plantsWeek2;
+console.log(plantAreaWeek2);
+// 32 meters
 
 // Week #3
 let plantsWeek3 = plantsWeek2 * 2;
 console.log(plantsWeek3);
 // 80 plants
 
-console.log(plantArea * plantsWeek3);
-// 51.20 meters
+const plantAreaWeek3 = plantArea * plantsWeek3;
+console.log(plantAreaWeek3);
+// 64 meters
 
 // Week 4
 let plantsWeek4 = plantsWeek3 * 2;
@@ -40,4 +39,35 @@ console.log(plantsWeek4);
 // 160 plants
 
 console.log(plantArea * plantsWeek4);
-// 102.40 meters (has exceeded the total area of 78.54 meters)
+// 128 meters (has exceeded the total area of 78.54 meters)
+
+const maxPlants = area / plantArea;
+console.log(maxPlants);
+// maximum number of plants is 98 plants
+
+if (plantAreaWeek1 >= area * 0.8) {
+  console.log("Time to Prune!");
+} else if (plantAreaWeek1 > area * 0.5 && plantAreaWeek1 < area * 0.8) {
+  console.log("Monitor the plants!");
+} else plantAreaWeek1 < area * 0.5;
+{
+  console.log("There is room for more plants");
+}
+
+if (plantAreaWeek2 >= area * 0.8) {
+  console.log("Time to Prune!");
+} else if (plantAreaWeek2 > area * 0.5 && plantAreaWeek2 < area * 0.8) {
+  console.log("Monitor the plants!");
+} else plantAreaWeek2 < area * 0.5;
+{
+  console.log("There is room for more plants");
+}
+
+if (plantAreaWeek3 >= area * 0.8) {
+  console.log("Time to Prune!");
+} else if (plantAreaWeek3 > area * 0.5 || plantAreaWeek3 < area * 0.8) {
+  console.log("Monitor the plants!");
+} else plantAreaWeek3 < area * 0.5;
+{
+  console.log("There is room for more plants");
+}
